e158e847a017c56005eb788524618660ff0d87db
lunamellow <nore0404@gmail.com>
2024-02-16 20:17:22 +0100

    (Stable) Size and color balls done, smooth ball half way

--- Assigment 4/MainWindow.py
+++ Assigment 4/MainWindow.py
@@ -11,10 +11,12 @@
 """
 
 # Library Imports
+import math
 from pyglet import *
 from pyglet.app import *
 from pyglet.window import *
 from pyglet.graphics import *
+from pyglet.clock import *
 
 
 class MainWindow(Window):
@@ -28,16 +30,63 @@ class MainWindow(Window):
         # Window size
         self.width, self.height = self.get_size()
 
-        # Top 4 circles
-        self.red_ball =     shapes.Circle(x=20, y=self.height - 30,  radius=20, segments=50, color=(255, 0, 0),   batch=self.batch)
-        self.yellow_ball =  shapes.Circle(x=20, y=self.height - 80,  radius=20, segments=50, color=(255, 255, 0), batch=self.batch)
-        self.green_ball =   shapes.Circle(x=20, y=self.height - 130, radius=20, segments=50, color=(0, 255, 0),   batch=self.batch)
-        self.blue_ball =    shapes.Circle(x=20, y=self.height - 180, radius=20, segments=50, color=(0, 0, 255),   batch=self.batch)
+        # Interpolation parameters
+        self.size_t = 0
+        self.color_t = 0
+        self.smooth_t = 0
 
-        # Bottom 3 circles
-        self.size_ball =    shapes.Circle(x=50,  y=self.height - 300, radius=50, segments=100, color=(255, 0, 0), batch=self.batch)
-        self.color_ball =   shapes.Circle(x=150, y=self.height - 300, radius=50, segments=100, color=(255, 0, 0), batch=self.batch)
-        self.smooth_ball =  shapes.Circle(x=250, y=self.height - 300, radius=50, segments=100, color=(0, 255, 0), batch=self.batch)
+        # Top 4 balls
+        self.ball_red =     shapes.Circle(x=20, y=self.height - 30,  radius=20, segments=50, color=(255, 0, 0),   batch=self.batch)
+        self.ball_yellow =  shapes.Circle(x=20, y=self.height - 80,  radius=20, segments=50, color=(255, 255, 0), batch=self.batch)
+        self.ball_green =   shapes.Circle(x=20, y=self.height - 130, radius=20, segments=50, color=(0, 255, 0),   batch=self.batch)
+        self.ball_blue =    shapes.Circle(x=20, y=self.height - 180, radius=20, segments=50, color=(0, 0, 255),   batch=self.batch)
+
+        # Bottom 3 balls
+        self.ball_size =    shapes.Circle(x=50,  y=self.height - 300, radius=50, segments=100, color=(255, 0, 0), batch=self.batch)
+        self.ball_color =   shapes.Circle(x=150, y=self.height - 300, radius=50, segments=100, color=(255, 0, 0), batch=self.batch)
+        self.ball_smooth =  shapes.Circle(x=250, y=self.height - 300, radius=50, segments=100, color=(0, 255, 0), batch=self.batch)
+
+    def update_size(self, dt):
+        self.size_t += dt * 0.25
+        t = (self.size_t % 2) / 1
+        if t <= 1:
+            self.ball_size.radius = t * t * (3 - 2 * t) * 50
+        else:
+            t = 2 - t
+            self.ball_size.radius = (t * t * (3 - 2 * t)) * 50
+
+    def update_color(self, dt):
+        self.color_t += dt * 0.25
+        t = (self.color_t % 2) / 1
+
+        if t <= 1:
+            smoothstep = t * t * (3 - 2 * t)
+            red = int((1 - smoothstep) * 255)
+            green = int(smoothstep * 255)
+        else:
+            t = 2 - t
+            smoothstep = t * t * (3 - 2 * t)
+            red = int((1 - smoothstep) * 255)
+            green = int(smoothstep * 255)
+
+        self.ball_color.color = (red, green, 0)
+
+    def update_smoothstep(self, dt):
+        self.smooth_t += dt * 0.25
+        t = (self.smooth_t % 2) / 1
+        if t <= 1:
+            self.ball_smooth.radius = t * t * (3 - 2 * t) * 50
+            smoothstep = t * t * (3 - 2 * t)
+            red = int((1 - smoothstep) * 255)
+            green = int(smoothstep * 255)
+        else:
+            t = 2 - t
+            self.ball_smooth.radius = (t * t * (3 - 2 * t)) * 50
+            smoothstep = t * t * (3 - 2 * t)
+            red = int((1 - smoothstep) * 255)
+            green = int(smoothstep * 255)
+        self.ball_smooth.color = (red, green, 0)
+
 
     def on_draw(self):
 
@@ -45,7 +94,7 @@ class MainWindow(Window):
         window.clear()
         fps_display.draw()
 
-        # Draw all shapes
+        # Draw all balls
         self.batch.draw()
 
 
@@ -55,6 +104,11 @@ if __name__ == '__main__':
     window = MainWindow(caption="BPROG - Luna Sofie Bergh", width=500, height=500, resizable=False)
     window.set_mouse_visible(visible=True)
     fps_display = FPSDisplay(window)
+
+    # Update functions
+    schedule_interval(window.update_size, 1 / 60)
+    schedule_interval(window.update_color, 1 / 60)
+    schedule_interval(window.update_smoothstep, 1 / 60)
 
     # Run the application
     run()
